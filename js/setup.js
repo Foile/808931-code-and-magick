ce8cd5f609041f062879b962c6b78bd5508ab947
a.shishova <a.shishova@contact-sys.com>
2018-09-06 11:40:57 +0300

    Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var WIZARDS_COUNT = 5;
+var WIZARDS_COUNT = 4;
 
 var getRandomElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
